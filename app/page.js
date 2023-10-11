b1516e28e65dc41227516bcf4221350078c9f867
Rainzler Exagone <Yasser458891@gmail.com>
2023-10-11 23:07:55 +0200

    Complete

--- app/page.js
+++ app/page.js
@@ -110,12 +110,7 @@ export default function Home() {
         <section>
           <div>
                 <h3 className='text-3xl py-1 dark:text-gray-200 '>Portfolio</h3>
-                {/* <p className='text-md py-2 leading-8 text-gray-80 dark:text-gray-50'>
-                  Since the beginning of my journey as a freelance Developer, I've done remote work for <span className='text-teal-500'>agencies </span>
-                  consulted for <span className='text-teal-500'>startups </span>
-                  and collaborated with talented people to creat digital products 
-                  for both business and consumer use.
-                </p> */}
+                
                 <p className='text-md py-2 leading-8 text-gray-800 dark:text-gray-50'>
                   I offer from a wide range of services , including brand Database and programming .
                 </p>
@@ -127,18 +122,7 @@ export default function Home() {
               <div className='basis-1/3 flex-1'>
                 <Image src={Rental} className='rounded-lg object-cover' width={'100%'} height={'100%'} layout='responsive'/>
                 </div>
-              {/* <div className='basis-1/3 flex-1'>
-                <Image src={web3} className='rounded-lg object-cover' width={'100%'} height={'100%'} layout='responsive' />
-              </div>
-              <div className='basis-1/3 flex-1'> 
-                <Image src={web4} className='rounded-lg object-cover' width={'100%'} height={'100%'} layout='responsive' />
-              </div>
-              <div className='basis-1/3 flex-1'>
-                <Image src={web5} className='rounded-lg object-cover' width={'100%'} height={'100%'} layout='responsive'/>
-              </div>
-              <div className='basis-1/3 flex-1'>
-                <Image src={web6} className='rounded-lg object-cover' width={'100%'} height={'100%'} layout='responsive'/>
-              </div> */}
+              
             </div>
         </section>
       </main>
